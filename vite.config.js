a8948c2fd8208e22a29725d26346251fb12ae3be
Averie Michel <averiestrunk@gmail.com>
2025-08-26 23:19:09 -0500

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: 'my-portfolio', // <-- replace with your repo name
   plugins: [react()],
   server: {
     proxy: {
