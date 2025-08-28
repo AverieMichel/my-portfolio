aa933e5a8202dd255918355f6e3c5b9f3cd99374
Averie Michel <averiestrunk@gmail.com>
2025-08-27 22:19:57 -0500

    Fix Vite base path for Heroku

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/my-portfolio/', // <-- replace with your repo name
+  base: '/', // <-- replace with your repo name
   plugins: [react()],
   server: {
     proxy: {
