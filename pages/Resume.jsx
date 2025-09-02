393308cf5d4ccc24319eee7b20e4ae416de11047
Averie Michel <averiestrunk@gmail.com>
2025-09-02 17:43:56 -0500

    Update frontend

--- pages/Resume.jsx
+++ pages/Resume.jsx
@@ -51,7 +51,7 @@ function Resume() {
       </div>
       <div className="experience">
         <ScrollReveal>
-          <Experience />
+          {/* <Experience /> */}
           <motion.img
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
